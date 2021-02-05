ceb4cc31f42042da27085dc54c20c315d9ae8d9a
Olga Grigorieva <ogrigorieva@solit-clouds.ru>
2021-02-05 14:58:09 +0300

    Домашнее задание 2. Добавлен параметр для определения количества символов после запятой

--- js/main.js
+++ js/main.js
@@ -15,11 +15,11 @@ getRandomNumber(0, 100);
 // Функция возвращает случайное !число с плавающей точкой! из переданного диапазона включительно.
 // Округлить до одного знака после запятой
 
-let getRandomFloat = function (min, max) {
-  return (Math.random() * (max - min) + min).toFixed(1);
+let getRandomFloat = function (min, max, decimal) {
+  return (Math.random() * (max - min) + min).toFixed(decimal);
 }
 
-getRandomFloat(0, 100);
+getRandomFloat(0, 100, 1);
 
 // Функция, проверяющая максимальную длину строки
 

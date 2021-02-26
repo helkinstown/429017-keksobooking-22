b55bc28423528dfdf212225d244cd6b1f7c3de39
Olga Grigorieva <ogrigorieva@solit-clouds.ru>
2021-02-26 18:33:23 +0300

    Домашнее задание 5.1. Исправлены циклы, добавлены размеры фото

--- js/common/card.js
+++ js/common/card.js
@@ -1,9 +1,15 @@
 import { typeText, rentObject } from './data.js';
 
-// как добавить размеры фото? они же есть в стилях но не срабатывают
-// как скрыть элемент? как удалить див потомков. строка 24 и 25
-// правильно ли удаляются потомки перед отрисовкой? строка 27
+// не работает внутри функции remove() и если не document
+// как надо получить данные src, куда вставлять
 
+// Функция удаляем потомков родителя
+let removeAllChildren = function (container) {
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+  return container; // нужен ли тут return?
+}
 
 let createCard = function() {
   let cardElement = document.querySelector('template').content.querySelector('.popup').cloneNode(true);
@@ -17,42 +23,40 @@ let createCard = function() {
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + rentObject.offer.checkin + ', ' + 'выезд после ' + rentObject.offer.checkout;
   cardElement.querySelector('.popup__description').textContent = rentObject.offer.description;
 
-  // массив с фичами
+  // добавляем данные и отрисовываем features по количеству из массива
   let featuresContainer = cardElement.querySelector('.popup__features');
   featuresContainer.textContent = rentObject.offer.features;
 
-  if(rentObject.offer.features.length === 0) {
-    //featuresContainer.parentElement.removeChild(featuresContainer);
-    featuresContainer.style.display = 'none';
-  } else {
-    while (featuresContainer.firstChild) {
-      featuresContainer.firstChild.remove();
+  if(rentObject.offer.features.length !== 0) {
+    removeAllChildren(featuresContainer);
+    for(let i = 0; i < rentObject.offer.features.length; i++) {
+      let featureItem = document.createElement('li');
+      featureItem.className = 'popup__feature popup__feature--' + rentObject.offer.features[i];
+      featuresContainer.appendChild(featureItem);
     }
   }
 
-  for(let i = 0; i < rentObject.offer.features.length; i++) {
-    let featureItem = document.createElement('li');
-    featureItem.className = 'popup__feature popup__feature--' + rentObject.offer.features[i];
-    featuresContainer.appendChild(featureItem);
-  }
+  // считываем размеры фото из разметки
+  let photoObj = cardElement.querySelector('.popup__photo'); // находим img до перерисовки
+  let imgHeight = photoObj.style.height;
+  let imgWidth = photoObj.style.width; // не приходит значение из разметки (((
 
-  // массив с фото
+  // получаем данные в img src
   let photosContainer = cardElement.querySelector('.popup__photos');
-  photosContainer.src = rentObject.offer.photos;
+  let photoData = rentObject.offer.photos;
+  photosContainer.innerHTML = `<img src="${rentObject.offer.photos}">`; // отрисовывается если пустой
 
-  if(rentObject.offer.photos.length === 0) {
-    photosContainer.style.display = 'none';
-  } else {
-    while (photosContainer.firstChild) {
-      photosContainer.firstChild.remove();
+  if(photoData.length !== 0) {
+    removeAllChildren(photosContainer);
+    for(let i = 0; i < photoData.length; i++) {
+      let photoItem = document.createElement('img');
+      photoItem.classList.add('popup__photo');
+      photoItem.src = photoData[i];
+      // прописываем размеры фото
+      photoItem.style.width = imgWidth;
+      photoItem.style.height = imgHeight;
+      photosContainer.appendChild(photoItem);
     }
-  }
-
-  for(let i = 0; i < rentObject.offer.photos.length; i++) {
-    let photoItem = document.createElement('img');
-    photoItem.classList.add('popup__photo');
-    photoItem.src = rentObject.offer.photos[i];
-    photosContainer.appendChild(photoItem);
   }
 
   let mapBlock = document.querySelector('.map__canvas');

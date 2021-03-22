f30c64055005851259a371effbb5a0e0db748c20
Olga Grigoreva <helkinstown@gmail.com>
2021-03-22 19:02:49 +0300

    Домашнее задание 5.2. Формы - исправлено поведение поля Цена за ночь

--- js/common/forms.js
+++ js/common/forms.js
@@ -10,22 +10,19 @@ let adFormEvent = function () {
     formPriceForNight.setAttribute('min', '0');
     switch(evt.target.value) {
       case 'bungalow':
-        formPriceForNight.placeholder = '0';
         formPriceForNight.min = '0';
         break;
       case 'flat':
-        formPriceForNight.placeholder = '1000';
         formPriceForNight.min = '1000';
         break;
       case 'house':
-        formPriceForNight.placeholder = '5000';
         formPriceForNight.min = '5000';
         break;
       case 'palace':
-        formPriceForNight.placeholder = '10000';
         formPriceForNight.min = '10000';
         break;
     }
+    formPriceForNight.value = formPriceForNight.min;
   });
 
   formPriceForNight.addEventListener('input', function(evt) {
